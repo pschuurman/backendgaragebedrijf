998e79893299fe901368b6351d42cdb69f86588a
pschuurman <pschuurman@gmail.com>
2022-10-15 17:15:22 +0200

    basis

---
+++ src/test/java/com/example/eindopdracht/garage/EindopdrachtGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eindopdracht.garage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
