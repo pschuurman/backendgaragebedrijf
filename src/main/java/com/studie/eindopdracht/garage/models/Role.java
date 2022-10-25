6b9f6739f3138ae808e956e594b028f6a2bb4d62
pschuurman <pschuurman@gmail.com>
2022-10-25 20:41:50 +0200

    role user model and repos

---
+++ src/main/java/com/studie/eindopdracht/garage/models/Role.java
@@ -0,0 +1,29 @@
+package com.studie.eindopdracht.garage.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Collection;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    private String rolename;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users;
+
+    public String getRolename() {
+        return rolename;
+    }
+
+    public void setRolename(String rolename) {
+        this.rolename = rolename;
+    }
+
+
+}
